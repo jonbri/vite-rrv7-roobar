9e4eafd2e19d41ba5dc0deb6ae596f3262518890
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-01-17 15:07:12 -0500

    add roobar

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,3 +1,4 @@
+import { Heading } from "roobar";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
@@ -21,6 +22,7 @@ export function Welcome() {
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
+            <Heading level={2}>roobar</Heading>
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
               What&apos;s next?
             </p>
